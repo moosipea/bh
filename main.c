8fe63ee82b5e6704174f074322a1a2088f0ac859
Mattias Volt <mattiasmcvolt@gmail.com>
2024-12-07 12:31:00 +0200

    keyboard input

--- main.c
+++ main.c
@@ -1,60 +1,88 @@
 #define GLFW_INCLUDE_NONE
 
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
 
-#include "error_macro.h"
 #include "engine.h"
+#include "error_macro.h"
 #include "res/built_assets.h"
 
 #define TEST_SPRITES 64
 
 static struct bh_ctx {
-    int width;
-    int height;
-    GLFWwindow *window;
+    int width, height;
+    GLFWwindow* window;
     bh_program program;
     m4 projection_matrix;
+    float dt;
 
     struct bh_sprite_batch batch;
     struct bh_textures textures;
-    struct bh_sprite_ll *entities;
+    struct bh_sprite_ll* entities;
 
-    struct bh_sprite_ll *player_entity;
+    struct bh_sprite_ll* player_entity;
 
     GLuint64 bulb_texture;
 
-    int keys_held[GLFW_KEY_LAST];
+    bool keys_held[GLFW_KEY_LAST + 1];
 } g_ctx = {0};
 
-static void glfw_key_cb(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    
+static void
+glfw_key_cb(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    (void)window;
+    (void)scancode;
+    (void)mods;
+    if (action == GLFW_PRESS) {
+        g_ctx.keys_held[key] = true;
+    } else if (action == GLFW_RELEASE) {
+        g_ctx.keys_held[key] = false;
+    }
 }
 
-static inline bool init_glfw(struct bh_ctx *ctx) {
+static inline bool get_key(struct bh_ctx* ctx, int glfw_key) {
+    if (glfw_key < 0 || glfw_key > GLFW_KEY_LAST) {
+        error("Invalid key");
+        return false;
+    }
+    return ctx->keys_held[glfw_key];
+}
+
+static inline bool init_glfw(struct bh_ctx* ctx) {
     if (!glfwInit()) {
         error("GLFW initialization failed");
         return false;
     }
 
-    ctx->window = glfwCreateWindow(ctx->width, ctx->height, "Hello, world!", NULL, NULL);
+    ctx->window =
+        glfwCreateWindow(ctx->width, ctx->height, "Hello, world!", NULL, NULL);
     if (!ctx->window) {
         error("Window creation failed");
         return false;
     }
 
-
+    glfwSetKeyCallback(ctx->window, glfw_key_cb);
 
     glfwMakeContextCurrent(ctx->window);
     glfwSwapInterval(1);
-       
+
     return true;
 }
 
-static inline bool init_gl(struct bh_ctx *ctx) {
+static void GLAPIENTRY gl_error_cb(
+    GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+    const GLchar* message, const void* user_param
+) {
+    (void)source;
+    (void)id;
+    (void)length;
+    (void)user_param;
+    error("GL ERROR (type=0x%x, severity=0x%x): %s", type, severity, message);
+}
+
+static inline bool init_gl(struct bh_ctx* ctx) {
     if (!init_glfw(ctx)) {
         return false;
     }
@@ -65,14 +93,19 @@ static inline bool init_gl(struct bh_ctx *ctx) {
         return false;
     }
 
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(gl_error_cb, NULL);
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     return true;
 }
 
-static inline bool init_shaders(struct bh_ctx *ctx) {
-    if ((ctx->program = create_program((const GLchar*)ASSET_vertex, (const GLchar*)ASSET_fragment))) {
+static inline bool init_shaders(struct bh_ctx* ctx) {
+    if ((ctx->program = create_program(
+             (const GLchar*)ASSET_vertex, (const GLchar*)ASSET_fragment
+         ))) {
         glUseProgram(ctx->program);
         return true;
     }
@@ -83,29 +116,29 @@ static inline float uniform_rand(void) {
     return 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f;
 }
 
-static void test_entity_system(void *ctx, struct bh_sprite_entity *entity) {
-    (void) ctx;
+static void test_entity_system(void* ctx, struct bh_sprite_entity* entity) {
+    (void)ctx;
     entity->y -= 1.0f / 60.0f;
     if (entity->y <= -1.0f) {
         entity->y = 1.0f;
     }
 }
 
-static inline void spawn_test_entities(struct bh_ctx *ctx) {
+static inline void spawn_test_entities(struct bh_ctx* ctx) {
     for (size_t i = 0; i < TEST_SPRITES; i++) {
 
         struct bh_sprite sprite = {0};
-        sprite.texture_handle = ctx->bulb_texture;
+        sprite.texture_handle   = ctx->bulb_texture;
         m4_identity(sprite.transform);
 
         struct bh_sprite_entity entity = {
-            .sprite = sprite,
-            .x = uniform_rand(),
-            .y = uniform_rand(),
-            .z = uniform_rand(),
-            .scale_x = 0.05f,
-            .scale_y = 0.05f,
-            .scale_z = 0.05f,
+            .sprite   = sprite,
+            .x        = uniform_rand(),
+            .y        = uniform_rand(),
+            .z        = uniform_rand(),
+            .scale_x  = 0.05f,
+            .scale_y  = 0.05f,
+            .scale_z  = 0.05f,
             .callback = test_entity_system,
         };
 
@@ -113,42 +146,54 @@ static inline void spawn_test_entities(struct bh_ctx *ctx) {
     }
 }
 
-static void update_player_system(void *ctx, struct bh_sprite_entity *entity) {
-    (void) ctx;
-    (void) entity;
+static void update_player_system(void* context, struct bh_sprite_entity* entity) {
+    struct bh_ctx *ctx = context;
+
+    if (get_key(ctx, GLFW_KEY_A)) {
+        entity->x -= 1.0f * ctx->dt;
+    }
+    if (get_key(ctx, GLFW_KEY_D)) {
+        entity->x += 1.0f * ctx->dt;
+    }
 }
 
-static inline void spawn_player_entity(struct bh_ctx *ctx) {
+static inline void spawn_player_entity(struct bh_ctx* ctx) {
     struct bh_sprite sprite = {0};
-    sprite.texture_handle = textures_load(&ctx->textures, (void*)ASSET_player, sizeof(ASSET_player) - 1);
+    sprite.texture_handle   = textures_load(
+        &ctx->textures, (void*)ASSET_player, sizeof(ASSET_player) - 1
+    );
 
     struct bh_sprite_entity entity = {
-        .sprite = sprite,
-        .x = 0.0f,
-        .y = 0.0f,
-        .z = 0.0f,
-        .scale_x = 0.15f,
-        .scale_y = 0.15f,
-        .scale_z = 0.15f,
+        .sprite   = sprite,
+        .x        = 0.0f,
+        .y        = 0.0f,
+        .z        = 0.0f,
+        .scale_x  = 0.15f,
+        .scale_y  = 0.15f,
+        .scale_z  = 0.15f,
         .callback = update_player_system,
     };
 
     spawn_entity(&ctx->entities, entity);
 }
 
-static void update_projection_matrix(struct bh_ctx *ctx) {
+static void update_projection_matrix(struct bh_ctx* ctx) {
     m4_scale(ctx->projection_matrix, 1.0f, 1.0f, 1.0f);
 
     m4 projection;
     m4_ortho(projection, 1.0f, ctx->width, 1.0f, ctx->height, 0.001f, 1000.0f);
     m4_multiply(ctx->projection_matrix, projection);
 
-    GLint projection_matrix_uniform = glGetUniformLocation(ctx->program, "projection_matrix");
-    glUniformMatrix4fv(projection_matrix_uniform, 1, GL_FALSE, (const GLfloat*) ctx->projection_matrix);
+    GLint projection_matrix_uniform =
+        glGetUniformLocation(ctx->program, "projection_matrix");
+    glUniformMatrix4fv(
+        projection_matrix_uniform, 1, GL_FALSE,
+        (const GLfloat*)ctx->projection_matrix
+    );
 }
 
-static inline bool init_ctx(struct bh_ctx *ctx) {
-    ctx->width = 640;
+static inline bool init_ctx(struct bh_ctx* ctx) {
+    ctx->width  = 640;
     ctx->height = 480;
 
     if (!init_gl(ctx)) {
@@ -162,9 +207,11 @@ static inline bool init_ctx(struct bh_ctx *ctx) {
     ctx->batch = batch_init();
 
     struct bh_textures textures = {0};
-    ctx->textures = textures;
+    ctx->textures               = textures;
 
-    ctx->bulb_texture = textures_load(&ctx->textures, (void*)ASSET_star, sizeof(ASSET_star) - 1);
+    ctx->bulb_texture = textures_load(
+        &ctx->textures, (void*)ASSET_star, sizeof(ASSET_star) - 1
+    );
     if (!ctx->bulb_texture) {
         return false;
     }
@@ -177,14 +224,15 @@ static inline bool init_ctx(struct bh_ctx *ctx) {
     return true;
 }
 
-static inline void pre_frame(struct bh_ctx *ctx) {
+static inline void pre_frame(struct bh_ctx* ctx) {
+    glfwSetTime(0.0);
     glfwPollEvents();
-    
+
     int width, height;
     glfwGetFramebufferSize(ctx->window, &width, &height);
 
     if (width != ctx->width || height != ctx->height) {
-        ctx->width = width;
+        ctx->width  = width;
         ctx->height = height;
         glViewport(0, 0, ctx->width, ctx->height);
         update_projection_matrix(ctx);
@@ -194,11 +242,12 @@ static inline void pre_frame(struct bh_ctx *ctx) {
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-static inline void post_frame(struct bh_ctx *ctx) {
+static inline void post_frame(struct bh_ctx* ctx) {
     glfwSwapBuffers(ctx->window);
+    ctx->dt = (float)glfwGetTime();
 }
 
-static inline void main_loop(struct bh_ctx *ctx) {
+static inline void main_loop(struct bh_ctx* ctx) {
     while (!glfwWindowShouldClose(ctx->window)) {
         pre_frame(ctx);
 
